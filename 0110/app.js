c3a51bd7257a7ba57cdec6baa2f42c202f162aab
sbqq <dvigds123123@gmail.com>
2025-01-10 16:18:58 +0800

    完成12小時轉換

---
+++ 0110/app.js
@@ -0,0 +1,17 @@
+// const currentDate = new Date();
+//
+// // 取得小時、分鐘和秒數
+// let currentHours = currentDate.getHours();
+// const currentMinutes = currentDate.getMinutes();
+// const currentSeconds = currentDate.getSeconds();
+//
+// // 設定上午或下午
+// let ampm = (currentHours >= 12) ? '下午' : '上午';
+//
+// // 轉換成12小時制
+// if (currentHours > 12) {
+//     currentHours  -=12;
+// }
+//
+// // 顯示時間
+// alert(`現在時間是 ${ampm} ${currentHours} 點 ${currentMinutes} 分 ${currentSeconds} 秒`);
